66b86cea20fcc358f3e3f6f4c660b43d57d2079c
leolind <leolind@umich.edu>
2024-10-11 17:25:06 -0400

    World map added

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,16 @@
 import { Welcome } from "../components/Welcome/Welcome";
-import { ColorSchemeToggle } from "../components/ColorSchemeToggle/ColorSchemeToggle";
 import { ProjectGrid } from "../components/ProjectGrid/ProjectGrid";
+import { WorldMap } from "../components/WorldMap/WorldMap"
 import { Text, Space } from "@mantine/core";
 
 export default function HomePage() {
   return (
     <>
       <Welcome />
-      {/* <ColorSchemeToggle /> */}
       <Space h={100} />
       <ProjectGrid />
+      <Space h={100} />
+      <WorldMap/>
     </>
   );
 }
